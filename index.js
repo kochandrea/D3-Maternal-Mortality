4f6493219193ad706718a41fe1b9ce5e9507bd25
kochandrea <35736047+kochandrea@users.noreply.github.com>
2019-03-05 18:05:51 -0600

    during OH

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ function myVis(data) {
 
   // set the dimensions and margins of the graph
   var margin = {top: 50, right: 50, bottom: 50, left: 50},
-      width = 800 - margin.left - margin.right,
-      height = 800 - margin.top - margin.bottom;
+      width = 900 - margin.left - margin.right,
+      height = 900 - margin.top - margin.bottom;
 
 
   // append the svg object to the body of the page
@@ -41,15 +41,44 @@ function myVis(data) {
 
 
 
-  // Step 3  (https://bl.ocks.org/syntagmatic/8ab9dc27f144683bc015eb4a2639d234)
-  console.log('prep data')
+  // Step 1  (https://bl.ocks.org/syntagmatic/8ab9dc27f144683bc015eb4a2639d234)
+  console.log('prep data');
+  console.log('step 1');
   const dataReady2 = d3.nest()
+    .key(function(d) { return d.year; })
+    // .rollup(function(v) { return d3.sum(v, function(d) { return d.rank; }); })
+    .object(high_inc_matmort);
+  // console.log(JSON.stringify(dataNest1));
+  console.log(dataReady2);
+
+  // Step 2  (https://bl.ocks.org/syntagmatic/8ab9dc27f144683bc015eb4a2639d234)
+  console.log('step 2');
+  const dataReady3 = d3.nest()
+    .key(function(d) { return d.year; })
+    .key(function(d) { return d.iso; })
+    .rollup(function(v) { return d3.sum(v, function(d) { return d.rank; }); })
+    .object(high_inc_matmort);
+  // console.log(JSON.stringify(dataNest1));
+  console.log(dataReady3);
+
+  // Step X  (https://bl.ocks.org/syntagmatic/8ab9dc27f144683bc015eb4a2639d234)
+  console.log('step x');
+  const dataReadyx = d3.nest()
     .key(function(d) { return d.iso; })
     .key(function(d) { return d.year; })
     .rollup(function(v) { return d3.sum(v, function(d) { return d.rank; }); })
-    .entries(high_inc_matmort);
+    .object(high_inc_matmort);
   // console.log(JSON.stringify(dataNest1));
-  console.log(dataReady2);
+  const updatedData = Object.entries(dataReadyx).map(([countryCode, yearDict]) => {
+    return {
+      iso: countryCode,
+      values: Object.entries(yearDict).map(([year, rank]) => {
+        return {year, rank};
+      })
+    };
+  });
+  console.log(updatedData);
+
 
   var dataReady = [
     {"iso": "USA",
@@ -74,12 +103,10 @@ function myVis(data) {
       {"year":10, "rank":12}]
     }];
 
-  console.log(dataReady);
-
-
   // Add X axis, but make invisible;
+  // maybe uswe scaleband
   var x = d3.scaleLinear()
-    .domain([0,10])
+    .domain([0, 10])
     .range([ margin.left, width ]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")");
@@ -98,17 +125,16 @@ function myVis(data) {
     .y(function(d) { return y(+d.rank) });
 
 
+    // myLines should be a real selector, eg .country-line
   svg.selectAll("myLines")
     .data(dataReady)
     .enter()
     .append("path")
-      .attr("class", function(d){ return d.iso })
+      .attr("class", function(d){ return `country-line ${d.iso}` })
       .attr("d", function(d){ return line(d.values) } )
       .style("stroke-width", 4)
       .style("stroke", "black")
       .attr("fill", "none");
-
-
 
 
   // Add the points
@@ -123,11 +149,19 @@ function myVis(data) {
     .selectAll("myPoints")
     .data(function(d){ return d.values })
     .enter()
-    .append("circle")
-      .attr("cx", function(d) { return x(d.year) } )
-      .attr("cy", function(d) { return y(d.rank) } )
-      .attr("r", 5)
+    .append("rect")
+      .attr("x", function(d) { return x(d.year) - 20 / 2} )
+      .attr("y", function(d) { return y(d.rank) - 20 / 2} )
+      .attr("width", 20)
+      .attr("height", 20)
       .attr("stroke", "white")
+
+    // .append("circle")
+    //   .attr("cx", function(d) { return x(d.year) } )
+    //   .attr("cy", function(d) { return y(d.rank) } )
+    //   .attr("r", 5)
+    //   .attr("stroke", "white")
+
 
   // Add a label at the beginning of each line
   svg
