dd02249bd7dda38d2f3dfc63d1b20b95644a2108
kochandrea <35736047+kochandrea@users.noreply.github.com>
2019-03-07 00:16:23 -0600

    generated more json files and attempted to change data in index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // use Promise.all to load in more than one dataset
   Promise.all([
+      './data/high_income.json',
       './data/high_income_matmort.json',
       './data/low_income_matmort.json',
       './data/lower_mid_income_matmort.json',
@@ -19,10 +20,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 function myVis(data) {
-  const [high_inc_matmort, low_inc_matmort,
+  const [high_income, high_inc_matmort, low_inc_matmort,
          lowermid_inc_matmort, uppermid_inc_matmort] = data;
 
-
+  console.log(high_income);
   // set the dimensions and margins of the graph
   var margin = {top: 50, right: 50, bottom: 50, left: 50},
       width = 900 - margin.left - margin.right,
@@ -39,13 +40,31 @@ function myVis(data) {
       .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
+  // const data1 = d3.nest()
+  //   .key( d => d.iso)
+  //   .key( d => d.year)
+  //   .rollup(function(v) { return d3.sum(v, function(d) { return d.rank; }); })
+  //   .rollup(function(v) { return d3.sum(v, function(d) { return d.name; }); })
+  //   .object(high_income);
+  // console.log('data1');
+  // console.log(data1);
+  // const data2 = Object.entries(data1).map(([countryCode, yearDict]) => {
+  //   return {
+  //     iso: countryCode,
+  //     values: Object.entries(yearDict).map(([year, rank]) => {
+  //       return {year, rank};
+  //     })
+  //   };
+  // });
+  // console.log('data2');
+  // console.log(data2);
+
   // Step X  (https://bl.ocks.org/syntagmatic/8ab9dc27f144683bc015eb4a2639d234)
-  console.log('step x');
   const dataReadyx = d3.nest()
     .key(function(d) { return d.iso; })
     .key(function(d) { return d.year; })
     .rollup(function(v) { return d3.sum(v, function(d) { return d.rank; }); })
-    .object(high_inc_matmort);
+    .object(high_income);
   const dataReady = Object.entries(dataReadyx).map(([countryCode, yearDict]) => {
     return {
       iso: countryCode,
@@ -54,6 +73,9 @@ function myVis(data) {
       })
     };
   });
+  console.log('dataReadyx');
+  console.log(dataReadyx);
+  console.log('dataReady');
   console.log(dataReady);
 
 
@@ -94,7 +116,7 @@ function myVis(data) {
     .append("path")
       .attr("class", d => `country-line ${d.iso}` )
       .attr("d", d => lineGenerator(d.values) )
-      .style("stroke-width", 4)
+      .style("stroke-width", 3)
       .style("stroke", "black")
       .attr("fill", "none");
 
