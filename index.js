66a0f023405f0c7da9618820956f97a98f78ca3f
kochandrea <35736047+kochandrea@users.noreply.github.com>
2019-03-08 17:21:20 -0600

    stuck on the dropdown

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     ].map(url => fetch(url).then(data => data.json())))
       .then(data => myVis(data))
     .catch(function(error){
-        console.log(`An unexpected error occured: ${error}`);
+        console.log(`An unexpected error occured: ${error}`,error);
     });
 });
 
@@ -23,6 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
 function myVis(data) {
 
   var [high_income, upper_mid_income, lower_mid_income, low_income] = data;
+
 
   // //button
   // d3.select("#button_high_income")
@@ -33,16 +34,48 @@ function myVis(data) {
   //   .on("click", function(d,i){
   //     generate_bumpchart(low_income)});
 
-
   // Create dropdown and dictionary
-  var dropdownDict = [{"income_level": "high_income", "dataset": high_income},
-                      {"income_level": "upper_mid_income", "dataset": upper_mid_income},
-                      {"income_level": "lower_mid_income", "dataset": lower_mid_income},
-                      {"income_level": "low_income", "dataset": low_income}
+  var dropdownDict = [{"income_level": "High income countries", "dataset": high_income},
+                      {"income_level": "Upper middle income countries", "dataset": upper_mid_income},
+                      {"income_level": "Lower middle income countries", "dataset": lower_mid_income},
+                      {"income_level": "Low income countries", "dataset": low_income}
                     ];
 
 
   console.log(dropdownDict);
+
+  // create the dropdown menu
+  var dropdownMenu = d3.select("#dropdownMenu")
+
+  dropdownMenu
+        .append("select")
+        .selectAll("option")
+        .data(dropdownDict)
+        .enter()
+        .append("option")
+        .attr("value", function(d){
+            return d.income_level;
+        })
+        .text(function(d){
+            return d.income_level;
+        });
+
+  dropdownMenu
+        .on('change', function(){
+        // Find which fruit was selected from the dropdown
+        var selected_income = d3.select(this)
+        .select("select")
+        .property("value")
+
+        console.log(selected_income)
+        // generate_bumpchart(selected_income)
+        // where income_level == selected_income
+        // dropdownDict.dataset ==
+
+        });
+
+  // Create the initial graph
+  generate_bumpchart(high_income)
 
   // Format data (citation:  https://bl.ocks.org/syntagmatic/8ab9dc27f144683bc015eb4a2639d234)
   function generate_bumpchart(selected_data){
@@ -97,38 +130,28 @@ function myVis(data) {
         svg.append("g")
           .call(d3.axisLeft(y));
 
-        // create the dropdown menu
-        var dropdownMenu = d3.select("#dropdownMenu")
-
-        dropdownMenu
-        	.append("select")
-        	.selectAll("option")
-              .data(dropdownDict)
-              .enter()
-              .append("option")
-              .attr("value", function(d){
-                  return d.income_level;
-              })
-              .text(function(d){
-                  return d.income_level;
-              });
-
-            dropdownMenu.on('change', function(){
-              	// Find which fruit was selected from the dropdown
-              	var selected_income = d3.select(this)
-                  .select("select")
-                  .property("value")
-
-                console.log(selected_income)
-
-                dropdownDict.dataset // where income_level == selected_income
-
-              });
-
         // create line generator
         var lineGenerator = d3.line().curve(d3.curveMonotoneX) // D3 Curve Explorer:  http://bl.ocks.org/d3indepth/b6d4845973089bc1012dec1674d3aff8
             .x( d => x(+d.year) )
-            .y( d => y(+d.rank) )
+            .y( d => y(+d.rank) );
+
+        // isoCodes = [];
+        // dataReady.forEach(function(d, i) {
+        //   isoCodes[i] = d.values[0].rank % 8;
+        // });
+        // console.log("ISO CODES");
+        // console.log(isoCodes);
+
+
+        var color = ['#13394A', //dark blue
+                '#E3DD44', //yellow
+                '#357797', //mid blue
+                '#D4626F', //salmon
+                '#948E00', //green
+                '#CC149B', //fuschia
+                '#7102FA', //purple
+                '#E48023' //orange
+              ];
 
         // generate lines
         svg
@@ -139,7 +162,7 @@ function myVis(data) {
             .attr("class", d => `country-line ${d.iso}` )
             .attr("d", d => lineGenerator(d.values) )
             .attr("stroke-width", 3)
-            .attr("stroke", "black")
+            .attr("stroke", function(d, i) { return color[i % 8];})
             .attr("fill", "none")
 
         // Add the points
@@ -153,26 +176,22 @@ function myVis(data) {
           // Second we need to enter in the 'values' part of this group
           .selectAll(".point-rect")
           .data( d => d.values)
-          // .data(function(d){ return d.values })
           .enter()
           .append("circle")
             .attr("cx", d => x(d.year))
             .attr("cy", d => y(d.rank))
-            // .attr("x", d => x(d.year) - 10 / 2 )
-            // .attr("y", d => y(d.rank) - 10 / 2 )
-            .attr("r", 5)
-            // .attr("width", 10)
-            // .attr("height", 10)
+            .attr("r", 1)
             .attr("stroke", "white")
-            .attr("fill", "red")
+            .attr("fill", "none")
             .on("mouseover", function() { focus.style("display", null); })
-            // .on("mouseout", function() { focus.style("display", "none"); })
             .on("mousemove", function(d) {
               var xPosition = d3.mouse(this)[0];
               var yPosition = d3.mouse(this)[1];
-              focus.attr("transform","translate(" + xPosition + "," + yPosition + ")");
+
+              focus.attr("transform","translate(" + xPosition + "," + yPosition + ")")
+
               focus.select("text")
-                    .text("Country: " + d.iso + "rank: " + d.rank + " year: " + d.year)
+                    .text("rank: " + d.rank + " year: " + d.year)
                     .attr("fill", "black")
             });
 
@@ -182,7 +201,10 @@ function myVis(data) {
             .style("display", "none");
 
         focus.append("circle")
-            .attr("r", 5);
+            .attr("r", 5)
+            .attr("fill", "#F4F4F4")
+            .attr("stroke-width", "4px")
+            .attr("stroke", function(d, i) { return color[i % 8];});
 
         focus.append("text")
           .attr("x", 15)
@@ -198,9 +220,10 @@ function myVis(data) {
               .attr("class", function(d){ return d.iso })
               .datum(function(d) { return {iso: d.iso, value: d.values[0]}; }) // keep only the last value of each time sery
               .attr("transform", function(d) { return "translate(" + x(d.value.year) + "," + y(d.value.rank) + ")"; }) // Put the text at the position of the last point
-              .attr("x", -40) // shift the text a bit more right
+              .attr("x", -30) // shift the text a bit more right
               .text(function(d) { return d.iso; })
               .attr("font-size", 10)
+              .attr("stroke", function(d, i) { return color[i % 8];})
 
         // Add a label at the end of each line
         svg
@@ -215,11 +238,11 @@ function myVis(data) {
               .attr("x", 12) // shift the text a bit more right
               .text(function(d) { return d.iso; })
               .attr("font-size", 10)
+              .attr("stroke", function(d, i) { return color[i % 8];})
 
       };
 
-    // Create the initial graph
-    generate_bumpchart(low_income)
+
 
     // Update the data
    	var updateGraph = function(dataset){
